7feeee1f4d806576d783c451c77dc5b2f3b8f368
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2022-11-22 16:15:53 +0000

    move vars

--- inputs-optional.tf
+++ inputs-optional.tf
@@ -21,3 +21,23 @@ variable "location" {
   type        = string
   default     = "uksouth"
 }
+
+variable "rules" {
+  description = "The rules to be applied to the resolver."
+  type = map(object({
+    domain_name = string
+    target_dns_servers = list(object({
+      use_resolver_inbound_address = bool
+      ip_address                   = string
+      port                         = number
+    }))
+    enabled = bool
+  }))
+  default = null
+}
+
+variable "dns_zone_vnets" {
+  description = "The IDs and Names of the VNets to link to the private DNS Resolver. These VNets should be linked to private dns zones."
+  type        = map(any)
+  default = null
+}
--- inputs-required.tf
+++ inputs-required.tf
@@ -46,21 +46,3 @@ variable "palo_alto_lb_ip" {
   description = "The IP address of the Palo Alto load balancer"
   type        = string
 }
-
-variable "rules" {
-  description = "The rules to be applied to the resolver."
-  type = map(object({
-    domain_name = string
-    target_dns_servers = list(object({
-      use_resolver_inbound_address = bool
-      ip_address                   = string
-      port                         = number
-    }))
-    enabled = bool
-  }))
-}
-
-variable "dns_zone_vnets" {
-  description = "The IDs and Names of the VNets to link to the private DNS Resolver. These VNets should be linked to private dns zones."
-  type        = map(any)
-}
